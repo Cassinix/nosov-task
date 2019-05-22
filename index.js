a04efa940f848593296afc689b9b42d75fe5449b
Cassinix <ninicassini@gmail.com>
2019-05-22 23:25:20 +0300

    fix: travis

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-'use strict'
+function hello() {
+  console.log('Hello Konstantin!');
+}
 
-const http = require('http')
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  res.write(req.url === '/' ? 'No request' : 'Request: ' + req.url)
-  res.end()
-}).listen(8080)
+hello();
