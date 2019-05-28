4e8313d3791a22fc904e0d397145b0ac1efbef48
Cassinix <ninicassini@gmail.com>
2019-05-28 22:13:23 +0300

    feat: link to shedule

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-trailing-spaces */
 const http = require('http');
 
-/*
+
 const jsdom = require('jsdom');
 
 const { JSDOM } = jsdom;
@@ -12,7 +13,7 @@ const dom = new JSDOM('', {
 
 const temp = dom.window.document.querySelector('h1');
 console.log(temp);
-*/
+
 
 /*
 https://api.telegram.org/bot844603049:AAHB5bM7onlavJ7blG0Y01hN2GBtmqmYlYU/getWebhookInfo
@@ -20,6 +21,6 @@ https://api.telegram.org/bot844603049:AAHB5bM7onlavJ7blG0Y01hN2GBtmqmYlYU/getWeb
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello World!');
+  res.write('Hello World');
   res.end();
 }).listen(8080);
