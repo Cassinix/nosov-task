81baf035b386c7f3bd23221daeaa8961af07c9fe
Cassinix <ninicassini@gmail.com>
2019-05-28 08:47:17 +0300

    feat: link to shedule

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-/* eslint-disable prefer-const */
-let http = require('http');
+const http = require('http');
+
+const jsdom = require('jsdom');
+
+const { JSDOM } = jsdom;
+
+const dom = new JSDOM('', {
+  url: 'http://rozklad.kpi.ua/Schedules/ViewSchedule.aspx?g=607599b2-3369-4bda-8320-803f33aac337',
+  contentType: 'text/html',
+});
+
+const temp = dom.window.document.querySelector('h1');
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello World!');
+  res.write(temp);
   res.end();
 }).listen(8080);
